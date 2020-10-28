217f1a1c9048af8572d1b16c5ac98deeffdb1956
elik <elik@example.com>
2020-10-28 22:10:48 +0200

    Загрузка файлів

---
+++ FileWork/Classes/Settings.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWork.Classes
+{
+    static class Settings
+    {
+        private static int counter = 0;
+        public static void ReadAllFiles(FileInfo[] files, string word) 
+        {
+            Console.WriteLine("FILES:");
+            
+            foreach (var file in files)
+            {
+                using (FileStream fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string allText = sr.ReadToEnd();
+
+                        if (allText.Contains(word))
+                        {
+                            int count = 0;
+                            string[] elements = allText.Split(new char[] { ' ' });
+                            foreach (var el in elements)
+                            {
+                                if (el == word)
+                                {
+                                    count++;
+                                }
+                            }
+
+                            if (count > 0) 
+                            {
+                               Console.WriteLine("=========================================");
+                               Console.WriteLine(fs.Name + " | " + count + " |");
+                               Console.WriteLine("=========================================");
+                                Settings.counter++;
+                            }
+
+
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void ReplaceWord(FileInfo[] files, string word) 
+        {
+            if (Settings.counter <= 0) 
+            {
+                return;
+            }
+            Console.Clear();
+            string newWord;
+            Console.Write("Write a new word or press 'Enter': ");
+            newWord = Console.ReadLine();
+            if (newWord.Length > 0)
+            {
+                foreach (var file in files)
+                {
+
+                    StreamReader sr = new StreamReader(file.FullName);
+                    string allText = sr.ReadToEnd();
+
+                    if (allText.Contains(word))
+                    {
+                        string[] elements = allText.Split(new char[] { ' ' });
+                        sr.Dispose();
+                        using (StreamWriter sw = new StreamWriter(file.FullName, false))
+                        {
+
+                            if (word.Length != 0)
+                            {
+                                for (int i = 0; i < elements.Length; i++)
+                                {
+                                    if (elements[i] == word)
+                                    {
+                                        elements[i] = newWord;
+                                    }
+                                }
+                            }
+
+                            string endStr = "";
+                            for (int i = 0; i < elements.Length; i++)
+                            {
+                                endStr += elements[i] + " ";
+                            }
+
+                            endStr.Trim(new char[] { ' ' });
+
+                            sw.Write(endStr);
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+
+}
---
+++ FileWork/Program.cs
@@ -0,0 +1,38 @@
+﻿using FileWork.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word;
+            string path;
+
+            Console.Write("Enter word: ");
+            word = Console.ReadLine();
+
+            Console.Write("Enter path to file: ");
+            path = Console.ReadLine();
+
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            FileInfo[] files = 
+                (directory.GetFiles()
+                .Where((FileInfo f) => { return f.Extension == ".txt"; })
+                .Select(f=>f))
+                .ToArray();
+
+            Console.Clear();
+
+            Settings.ReadAllFiles(files, word);
+            Settings.ReplaceWord(files, word);
+        }
+    }
+}
